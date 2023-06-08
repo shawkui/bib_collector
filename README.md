c2528b6792bbbd1bc1dee6db866cf6036541e9b5
shawkui <shaokui.w@gmail.com>
2023-06-08 23:38:44 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# bib_collector
-A automatic collector for BibTeX from Google Scholar
+## Bib Collector
+#### A simple python program to collect BibTeX info from Google Scholar.
+
+1. Install selenium
+>> pip install selenium==4.9.1 
+2. Install Chrome/Chromium and Chrome driver
+    * Install Chrome or Chromium
+    * Check your Chrome/Chromium version, e.g., enter ”chrome://version/“ in the address line
+    * Install the corresponding driver from http://chromedriver.storage.googleapis.com/index.html to your_customized_driver_path
+
+3. Modify settings in collect.py:
+
+    >> input_file = 'paper_list.txt'
+    
+    >> output_file = 'links.txt'
+    
+    >> output_bib_file = 'bib.txt'
+    
+    >> chromedriver_path = "your_customized_driver_path"
+
+4. Run in terminal
+    * Add the paper titles to input_file line by line.
+    * Run the following code:
+    >> python collect.py
+
+5. Exception
+   * After some running, the program may be interrupted by Google. Run the following line to continue to collect from line xxx
+   >> python collect.py xxx
+
---
+++ collect.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+import time 
+from selenium.webdriver.common.by import By
+
+import random
+import sys 
+
+# modified from https://zhuanlan.zhihu.com/p/448630104
+def extract_href(input_file, output_file, output_bib_file, chrome_driver, begin_index = 0):
+    '''
+    input_file: a txt file containing the paper titles
+    output_file: a txt file containing the links to the bibtex files
+    output_bib_file: a txt file containing the raw bibtex files
+    chrome_driver: the path to the chrome driver
+    begin_index: the index of the first paper. Used when the program is interrupted.
+    '''
+    scholars = open(input_file)
+    file_out = open(output_file,'a+')
+    file_bib_out = open(output_bib_file, 'a+')
+    scholars = scholars.readlines()
+    browser = webdriver.Chrome()
+    url = "https://scholar.google.com"
+    browser.get(url)
+    links = []
+    bibs = []
+    failed = []
+    current_line = begin_index
+
+    for tt in scholars[begin_index:]:
+        tt = tt.strip().split('\t')
+        print(tt[0])
+        tt = tt[-1]
+        browser.get(url)
+        time.sleep(random.uniform(0.5,1.5))
+
+        browser.find_element(by=By.XPATH, value = '//*[@name="q"]').send_keys(tt)
+        try:
+            browser.find_element(by=By.XPATH,value = '//*[@name="btnG"]').click()
+
+            browser.find_element(by=By.XPATH,value = '//*[@class="gs_or_cit gs_or_btn gs_nph"]').click()
+            time.sleep(random.uniform(0.5,1.5))
+    
+            link = browser.find_element(by=By.XPATH,value = '//*[@class="gs_citi"]').get_attribute('href')
+            print(link)
+            if link not in links:
+                links.append(link)
+                file_out.write(link+'\n')
+            
+            browser.get(link)
+            text = browser.find_element(by=By.XPATH,value = '/html/body/pre')  
+            text = text.text + '\n'
+            file_bib_out.writelines(text)
+            bibs.append(text)
+        except:
+            print('[*****************************]')
+            failed.append(tt)
+            continue
+        print('>> current line:', current_line)
+        current_line+=1
+
+    print(links)
+    print(bibs)
+    file_out.close()
+    file_bib_out.close()
+
+input_file = 'paper_list.txt'
+output_file = 'links.txt'
+output_bib_file = 'raw_bib.txt'
+chromedriver_path = "your_customer_driver_path"
+
+if len(sys.argv)>1:
+    print(sys.argv)
+    extract_href(input_file, output_file, output_bib_file, chromedriver_path, begin_index=int(sys.argv[1]))    
+else:
+    extract_href(input_file, output_file, output_bib_file, chromedriver_path)
